490fef82ff00e8224b30a5421f027ff8f306dd04
Avinash123-456 <53894583+Avinash123-456@users.noreply.github.com>
2020-04-13 18:58:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+	<!DOCTYPE html>
+	<html>
+	   <head>
+	      <title>Project 1</title>
+	      <link rel="stylesheet" type="text/css" href="style.css">
+	      <!-- Google Fonts -->
+	      <link href="https://fonts.googleapis.com/css?family=Acme&display=swap" rel="stylesheet">
+	      <link href="https://fonts.googleapis.com/css?family=Patua+One&display=swap" rel="stylesheet">
+	      <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+	      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	      <meta charset="utf-8">
+		  <!-- CSS including -->
+	      <link rel="stylesheet" href="index.css">
+	   </head>
+	   <body>
+	      <!-- Menu Bar -->
+	      <header>
+	         <nav>
+	            <div class="logo"><img src="https://images3.alphacoders.com/666/thumb-350-666452.png"></div>
+	            <div class="menu">
+	               <ul>
+	                  <a href="home"><li>Home</li></a>
+					  <a href="#about"><li>About Us</li></a>
+	                  <li>Contact Us</li>
+	                  <li>Information</li>
+	               </ul>
+	            </div>
+	         </nav>
+	      </header>
+	      <!-- Home -->
+		  <a href="Home"></a>
+	      <section class="home">
+	         <div class="content">
+	            <h1 class="header">! Welcome To Web !</h1>
+	            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries </p>
+	         </div>
+	      </section>
+	      <!-- About Us -->
+		  <h1 id="about">About Us</h1>
+	      <section class="about">
+	         <h1 class="heading">About Us</h1>
+	         <div class="about-us">
+	            <div class="image">
+	               <img src="https://images.pexels.com/photos/1236701/pexels-photo-1236701.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">				
+	            </div>
+	            <div class="paragraph">
+	               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	                  Why do we use it?
+	                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+	               </p>
+	            </div>
+	         </div>
+	      </section>
+	      <!-- Gallary -->
+	      <section class="gallary">
+	         <h1 class="heading">My Gallary</h1>
+	         <div class="images">
+	            <div class="img">
+	               <img src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+	            </div>
+	            <div class="img">
+	               <img src="https://images.unsplash.com/photo-1508672019048-805c876b67e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80">
+	            </div>
+	            <div class="img">
+	               <img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+	            </div>
+	            <div class="img">
+	               <img src="https://images.unsplash.com/photo-1503457574462-bd27054394c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+	            </div>
+	            <div class="img">
+	               <img src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+	            </div>
+	            <div class="img">
+	               <img src="https://images.unsplash.com/photo-1531219432768-9f540ce91ef3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+	            </div>
+	         </div>
+	      </section>
+	      <!-- Our Team -->
+	      <section class="team">
+	         <h1 >Our Technical Team</h1>
+	         <div class="our-team">
+	            <div class="box">
+	               <h2>Eleanor</h2>
+	               <img src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80">
+	               <h3>Front End Developer</h3>
+	            </div>
+	            <div class="box">
+	               <h2>Jacob</h2>
+	               <img src="https://images.unsplash.com/photo-1549068106-b024baf5062d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80">
+	               <h3>Full Stack Developer</h3>
+	            </div>
+	            <div class="box">
+	               <h2>Oliver</h2>
+	               <img src="https://images.unsplash.com/photo-1547624643-3bf761b09502?ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80">
+	               <h3>Back End Developer</h3>
+	            </div>
+	         </div>
+	      </section>
+
+	      <!-- Contact Us -->
+	      <section class="contact">
+	      		<h1>Contact Us</h1>
+	      		<div class="form">
+	      			<div class="form-group">
+	      				<input type="text" name="name" placeholder="Enter Your Name">
+
+	      			</div>
+	      			<div class="form-group">
+	      				<input type="email" name="email" placeholder="Enter Your Email">
+	      				
+	      			</div>
+	      			<div class="form-group">
+	      				<input type="text" name="Contact" placeholder="Enter Your Contact">
+	      				
+	      			</div>
+	      			<button type="submit">SUBMIT</button>
+	      		</div>
+	      </section>
+	      <!-- Footer -->
+	      <footer class="footer">
+	         <div class="foot">
+	            Copyright&copy; 1995-2020 eBay Inc. All Rights Reserved. 
+	         </div>
+	      </footer>
+	   </body>
+	</html>
